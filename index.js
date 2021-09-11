6f80870304016c5a09ad4702ec4a483e90adb31b
sajjad sadiq <sajjadsadiq2020@gmail.com>
2021-09-11 23:56:52 +0600

    update server site

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 const express = require('express');
 const app = express();
+const dotenv = require('dotenv')
+const mongoose = require('mongoose')
+
+dotenv.config()
+mongoose.connect('mongodb://localhost:27017/test', {
+    useNewUrParser: true,
+    useUnifiedTopology: true,
+
+});
 
 app.listen("5000", ()=>{
     console.log("Server site is running");
